87960dc31104b5a8b3eecc17ee95881355fd0bba
Nick <nickseldner@gmail.com>
2022-04-29 01:16:26 -0400

    Refactored most of CRUD functions, added get_honeypot, delete_honeypot, and update_honeypot

--- gethoney/crud.py
+++ gethoney/crud.py
@@ -15,6 +15,13 @@ class Database:
             """CREATE TABLE IF NOT EXISTS honeypots
         (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, url TEXT, description TEXT)"""
         )
+        self.curr.execute(
+            """CREATE TABLE IF NOT EXISTS logs
+             (id INTEGER PRIMARY KEY AUTOINCREMENT,
+              name TEXT,
+              honeypot_id INTEGER,
+              FOREIGN KEY (honeypot_id) REFERENCES honeypots (id))"""
+        )
 
     def insert_into_db(self, honeypots: list[Honeypot]):
         self.curr.executemany(
@@ -25,6 +32,8 @@ class Database:
         )
 
         self.conn.commit()
+
+        # TODO: Return exactly what's being inserted into DB, which includes auto incremented ID
         # self.curr.execute(
         #     """SELECT id FROM honeypots WHERE
         #             (name) == VALUES (?) """,
@@ -36,25 +45,59 @@ class Database:
     # TODO: Make Select * dynamic from list in initiator
 
     def select_from_db(self):
-        self.curr.execute("SELECT * FROM honeypots")
-        data = self.curr.fetchall()
+        data = self.curr.execute("SELECT * FROM honeypots").fetchall()
         return data
 
     def retrieve_logs(self, honeypot: Honeypot):
-        logs = []
 
-        fetch = self.curr.execute("""SELECT url FROM honeypots WHERE name == ? """, (honeypot,)).fetchall()
-        url = fetch[0][0]
+        fetch_url_id = self.curr.execute("SELECT url, id FROM honeypots WHERE name == ?", (honeypot.name,)).fetchall()
+
+        url = fetch_url_id[0][0]
+        id = fetch_url_id[0][1]
 
         request = requests.get(url)
         data = request.json()
 
-        for item in data:
-            logs.append(item["name"])
-        return logs
+        self.curr.executemany(
+            """INSERT INTO logs
+            (name, honeypot_id)
+            VALUES (?, ?)""",
+            [(item["name"], id) for item in data],
+        )
+        self.conn.commit()
+
+    def update_honeypot(self, _id: int, honeypot: Honeypot, name=None, url=None, description=None):
+
+        params = [name or honeypot.name, url or honeypot.url, description or honeypot.description, _id]
+
+        self.curr.execute("UPDATE honeypots SET name = ?, url = ?, description = ? WHERE id == ?", (params))
+        self.conn.commit()
+
+    def delete_honeypot(self, _id: int):
+        try:
+            self.curr.execute(
+                """DELETE FROM honeypots
+                WHERE id == ?""",
+                (_id,),
+            )
+            self.conn.commit()
+        except IndexError:
+            return "No honeypots with that id."
+
+    def get_honeypot(self, _id: int):
+        data = self.curr.execute("SELECT * FROM honeypots WHERE id == ?", (_id,)).fetchall()
+        return data
 
 
 # gethoney_db = "../data/gethoney.db"
 # f = Database(gethoney_db)
+# # honeypot = Honeypot(name="aws", url="http://3.137.141.78", description="test1")
+# hp = Honeypot(name="bob", url="http://1.1.1.1", description=" ")
+# # print(f.select_from_db())
+# # f.delete_honeypot("test1")
+# f.update_honeypot(hp, name="bob1", url="http://13.1.1.1", description="test2")
+# # print(f.select_from_db())
 
-# print(f.retrieve_logs("test1"))
+# # print(honeypot.url)
+
+# # print(f.retrieve_logs(honeypot))
--- gethoney/main.py
+++ gethoney/main.py
@@ -9,12 +9,6 @@ db = Database(gethoney_db)
 app = FastAPI()
 
 
-# curr.execute(
-#     """CREATE TABLE IF NOT EXISTS logs
-#     (id INTEGER PRIMARY KEY, log_id INTEGER, name TEXT))"""
-# )
-
-
 @app.post("/honeypots/", status_code=201)
 def create_honeypots(honeypots: list[Honeypot]):
     db.insert_into_db(honeypots)
@@ -26,9 +20,17 @@ def get_honeypots():
     return db.select_from_db()
 
 
-# @app.get("/honeypots/logs/")
-# def get_logs(honeypot: list[], )
+@app.delete("/honeypots/{honeypot_id}", status_code=200)
+def delete_honeypot(honeypot_id: int):
+    return db.delete_honeypot(id)
 
 
-# @app.post("/honeypots/logs/")
-# def create_logs(honeypot: Honeypot):
+@app.put("/honeypots/{honeypot_id}", status_code=200)
+def update_honeypot(honeypot_id: int, honeypot: Honeypot, name=None, url=None, description=None):
+    return db.update_honeypot(honeypot_id, honeypot, name, url, description)
+
+
+@app.get("/honeypots/{id}", status_code=200)
+def get_honeypot(id: int):
+    print(id)
+    return db.get_honeypot(id)

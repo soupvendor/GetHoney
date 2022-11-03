26f30c0553ecad8b8941e53faef08bafddc72303
Nick Seldner <nickseldner@gmail.com>
2022-11-03 14:26:47 -0400

    initial setup of react frontend

---
+++ ui/frontend/src/App.js
@@ -0,0 +1,30 @@
+import './App.css';
+import logo from './logo.svg';
+
+function WelcomeMessage() {
+  return <p>Welcome!</p>
+}
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <WelcomeMessage />
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
